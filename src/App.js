7a0a37a4a492abbcc7b62f2e2be70b3984f10e56
BartoszJS <bartosz290300@wp.pl>
2023-03-07 17:11:32 +0100

    25% first hanoi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [firstColumn, setFirstColumn] = useState([1, 2, 3]);
+  const [firstColumnPlaceholder, setFirstColumnPlaceholder] = useState([]);
+  const [secondColumn, setSecondColumn] = useState([]);
+  const [thirdColumn, setThirdColumn] = useState([]);
+
+  const handleFirstPlaceholder = (firstColumn) => {
+    if (firstColumnPlaceholder.length === 0) {
+      if (firstColumn) {
+        setFirstColumnPlaceholder(firstColumn[0]);
+        firstColumn.shift();
+      }
+    } else {
+      console.log("hello");
+    }
+  };
+  const deleteFirstPlaceholder = () => {
+    setFirstColumnPlaceholder([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='content'>
+        <div className='content-holder'>
+          <div
+            className='firstColumnPlaceholder'
+            onClick={() => deleteFirstPlaceholder(firstColumnPlaceholder)}
+          >
+            {firstColumnPlaceholder}
+          </div>
+          <div
+            onClick={() => handleFirstPlaceholder(firstColumn)}
+            className='singleColumn'
+          >
+            {firstColumn.map((value) => (
+              <div className={`column${value}`} key={value}>
+                {value}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='singleColumn'>
+          {secondColumn.map((value) => (
+            <div key={value}>{value}</div>
+          ))}
+        </div>
+        <div className='singleColumn'>
+          {thirdColumn.map((value) => (
+            <div key={value}>{value}</div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

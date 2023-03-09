7d1c22855f8f7b90b893829b7b8dcf3d03e6fedb
BartoszJS <bartosz290300@wp.pl>
2023-03-09 13:32:00 +0100

    55% mechanism works

--- src/App.js
+++ src/App.js
@@ -15,46 +15,104 @@ function App() {
         firstColumn.shift();
       }
     } else {
-      console.log("hello");
+      let arr = [firstColumnPlaceholder];
+      let fullArr = [];
+      console.log(firstColumn[0]);
+      if (firstColumnPlaceholder > firstColumn[0]) {
+        alert("Nie możesz położyć większego na mniejszy");
+      } else {
+        setFirstColumn(fullArr.concat(arr, firstColumn));
+        setFirstColumnPlaceholder([]);
+      }
     }
   };
-  const deleteFirstPlaceholder = () => {
-    let arr = [firstColumnPlaceholder];
-    let fullArr = [];
-    setFirstColumn(fullArr.concat(arr, firstColumn));
-    setFirstColumnPlaceholder([]);
+
+  const handleSecondPlaceholder = (secondColumn) => {
+    if (firstColumnPlaceholder.length === 0) {
+      if (secondColumn) {
+        setFirstColumnPlaceholder(secondColumn[0]);
+        secondColumn.shift();
+      }
+    } else {
+      let arr = [firstColumnPlaceholder];
+      let fullArr = [];
+
+      if (firstColumnPlaceholder > secondColumn[0]) {
+        alert("Nie możesz położyć większego na mniejszy");
+      } else {
+        setSecondColumn(fullArr.concat(arr, secondColumn));
+        setFirstColumnPlaceholder([]);
+      }
+    }
+  };
+  const handleThirdPlaceholder = (thirdColumn) => {
+    if (firstColumnPlaceholder.length === 0) {
+      if (thirdColumn) {
+        setFirstColumnPlaceholder(thirdColumn[0]);
+        thirdColumn.shift();
+      }
+    } else {
+      let arr = [firstColumnPlaceholder];
+      let fullArr = [];
+
+      if (firstColumnPlaceholder > thirdColumn[0]) {
+        alert("Nie możesz położyć większego na mniejszy");
+      } else {
+        setThirdColumn(fullArr.concat(arr, thirdColumn));
+        setFirstColumnPlaceholder([]);
+      }
+    }
   };
 
   return (
     <div className='App'>
+      <div className='firstColumnPlaceholder flex h-[40px] justify-center items-center'>
+        {firstColumnPlaceholder == 1 && (
+          <div className='bg-blue-600 w-[20px]'>{firstColumnPlaceholder}</div>
+        )}
+        {firstColumnPlaceholder == 2 && (
+          <div className='bg-yellow-400 w-[30px]'>{firstColumnPlaceholder}</div>
+        )}
+        {firstColumnPlaceholder == 3 && (
+          <div className='bg-green-700 w-[40px]'>{firstColumnPlaceholder}</div>
+        )}
+      </div>
       <div className='content'>
         <div className='content-holder'>
           <div
-            className='firstColumnPlaceholder'
-            onClick={() => deleteFirstPlaceholder(firstColumnPlaceholder)}
-          >
-            {firstColumnPlaceholder}
-          </div>
-          <div
             onClick={() => handleFirstPlaceholder(firstColumn)}
-            className='singleColumn'
+            className='singleColumn cursor-pointer cursor-pointer'
           >
             {firstColumn.map((value) => (
+              <div className={` column${value}`} key={value}>
+                {value}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='content-holder'>
+          <div
+            onClick={() => handleSecondPlaceholder(secondColumn)}
+            className='singleColumn cursor-pointer'
+          >
+            {secondColumn.map((value) => (
               <div className={`column${value}`} key={value}>
                 {value}
               </div>
             ))}
           </div>
         </div>
-        <div className='singleColumn'>
-          {secondColumn.map((value) => (
-            <div key={value}>{value}</div>
-          ))}
-        </div>
-        <div className='singleColumn'>
-          {thirdColumn.map((value) => (
-            <div key={value}>{value}</div>
-          ))}
+        <div className='content-holder'>
+          <div
+            onClick={() => handleThirdPlaceholder(thirdColumn)}
+            className='singleColumn cursor-pointer'
+          >
+            {thirdColumn.map((value) => (
+              <div className={`column${value}`} key={value}>
+                {value}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
